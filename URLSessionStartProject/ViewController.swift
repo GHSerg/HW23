bbe8352084f6d8578d38b7e1d6b507ad5e447021
Serg <your@serg.com>
2022-06-24 12:23:59 +0500

    Calculate the hash using MD5

--- URLSessionStartProject/ViewController.swift
+++ URLSessionStartProject/ViewController.swift
@@ -6,9 +6,10 @@
 //
 
 import UIKit
+import CommonCrypto
 
 class ViewController: UIViewController {
-
+    
     private let endpointClient = EndpointClient(applicationSettings: ApplicationSettingsService())
 
     override func viewDidLoad() {
@@ -40,18 +41,22 @@ class ViewController: UIViewController {
 
 final class GetNameEndpoint: ObjectResponseEndpoint<String> {
     
+    let publicKey = "96732abd690b89d71a8daeccb564a4be"
+    let privateKey = "6713c39a1931bb5bfef0fe63be2eb22bb3aa096a"
+    let tsValue = "1"
+    
     override var method: RESTClient.RequestType { return .get }
     override var path: String { "/v1/public/characters/1010743/series" }
     
     override init() {
         super.init()
+        
 
-        queryItems = [URLQueryItem(name: "ts",value: "1"),
-                      URLQueryItem(name: "apikey",value: "96732abd690b89d71a8daeccb564a4be"),
-                      URLQueryItem(name: "hash",value: "debf0b805f2e5c9766ec3467673189ee"),
+        queryItems = [URLQueryItem(name: "ts",value: tsValue),
+                      URLQueryItem(name: "apikey",value: publicKey),
+                      URLQueryItem(name: "hash",value: (tsValue + privateKey + publicKey).md5),
         ]
     }
-    
 }
 
 
@@ -72,3 +77,17 @@ func decodeJSONOld() {
         print("Failed to load: \(error.localizedDescription)")
     }
 }
+
+extension String {
+    var md5: String {
+        let data = Data(self.utf8)
+        let hash = data.withUnsafeBytes { (bytes: UnsafeRawBufferPointer) -> [UInt8] in
+            var hash = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+            CC_MD5(bytes.baseAddress, CC_LONG(data.count), &hash)
+            return hash
+        }
+        return hash.map { String(format: "%02x", $0) }.joined()
+    }
+}
+
+

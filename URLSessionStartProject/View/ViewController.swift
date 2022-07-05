bf99ac371a61d14bbc9c5c19787c9639c220d257
Serg <your@serg.com>
2022-07-05 21:42:22 +0500

    md5 refactoring

--- URLSessionStartProject/View/ViewController.swift
+++ URLSessionStartProject/View/ViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-import CommonCrypto
+import CryptoKit
 
 class ViewController: UIViewController {
     
@@ -79,14 +79,10 @@ func decodeJSONOld() {
 }
 
 extension String {
-    var md5: String {
-        let data = Data(self.utf8)
-        let hash = data.withUnsafeBytes { (bytes: UnsafeRawBufferPointer) -> [UInt8] in
-            var hash = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
-            CC_MD5(bytes.baseAddress, CC_LONG(data.count), &hash)
-            return hash
-        }
-        return hash.map { String(format: "%02x", $0) }.joined()
+    var md5: String? {
+        guard let data = self.data(using: .utf8) else { return nil }
+        let computed = Insecure.MD5.hash(data: data)
+        return computed.map { String(format: "%02x", $0) }.joined()
     }
 }
 
